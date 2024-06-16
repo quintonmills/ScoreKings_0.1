29c727bb1da6b06466e53fb79b242a9340380393
quintonmills <quintonmills98@gmail.com>
2024-06-16 09:02:56 -0400

    edit tile icon

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -28,7 +28,7 @@ export default function TabLayout() {
         options={{
           title: 'Draft',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            <TabBarIcon name={focused ? 'stopwatch' : 'stopwatch-outline'} color={color} />
           ),
         }}
       />
