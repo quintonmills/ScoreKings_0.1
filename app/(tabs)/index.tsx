984fcbbc65f430b3692ed9edbf69a2be9b119c8b
quintonmills <quintonmills98@gmail.com>
2024-06-17 23:05:58 -0400

    create ranking view for players

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -20,32 +20,18 @@ export default function HomeScreen() {
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+        <ThemedText type="subtitle">1st</ThemedText>
+        <ThemedText type="subtitle">2nd</ThemedText>
+        <ThemedText type="subtitle">3rd</ThemedText>
+        <ThemedText type="subtitle">4th</ThemedText>
+        <ThemedText type="subtitle">5th</ThemedText>
+        <ThemedText type="subtitle">6th</ThemedText>
         <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
+          Edit 
         </ThemedText>
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+     
+      
     </ParallaxScrollView>
   );
 }
